61efec0b5b01efdd5f50143d184262147493b7be
LuisGuardia <g.luisalberto68@gmail.com>
2019-06-17 16:59:19 -0400

    task3 devops

--- devops/environments.bat
+++ devops/environments.bat
@@ -3,6 +3,7 @@ set OS_AUTH_URL=http://10.28.3.240:5000/v3
 set OS_IDENTITY_API_VERSION=3
 set OS_TENANT_NAME=2099bc1dc3ec4c76a9cdbab47ea675a7
 set OS_PROJECT_NAME=fj_at09
+set OS_PRIVATE_KEY_PATH=C:\\Users\\Admin\\Desktop\\key\\luisguardia.pem
 set OS_USERNAME=luisguardia
 set OS_PASSWORD=?????????
 set OS_DOMAIN_NAME=fundacion-jala
